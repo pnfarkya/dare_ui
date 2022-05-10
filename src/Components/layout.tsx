0025387a3a3455cc1fad31fbbfd883d498ef02b1
Harshal <harshalparab@gmail.com>
2022-05-11 00:13:36 +0530

    Added header footer and layout.

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -2,8 +2,7 @@ import Header from "./header";
 import Footer from "./footer";
 import styles from '../../styles/Home.module.css';
 
-
-const Layout = props => (
+const Layout = (props) => (
     <div className={styles.layout}>
       <Header />
       <div className={styles.layout}>
--- src/home.tsx
+++ src/home.tsx
@@ -2,21 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from './Components/layout'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>DARE</title>
-        <meta name="description" content="Dare" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to DARE HOME
-        </h1>
-      </main>      
+      
     </div>
   )
 }
