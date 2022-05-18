0fdbf4bbc5b1196a50f761fdad1771c9b68c2c09
Harshal <harshalparab@gmail.com>
2022-05-18 12:43:34 +0530

    added click area for cards and width adjustment

--- src/Components/common/nftCollectionDisplay.tsx
+++ src/Components/common/nftCollectionDisplay.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { makeStyles } from "@mui/styles";
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import CardActionArea from '@mui/material/CardActionArea';
 import ImageStack from '../common/imageStack';
-import { border, fontFamily, spacing, textAlign } from "@mui/system";
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import { wrap } from "module";
 import { Carousel } from '../customControls/carouselControl'
 
 class NftCollectionDisplay extends React.Component<any, any> {
@@ -38,13 +31,15 @@ class NftCollectionDisplay extends React.Component<any, any> {
         <Grid container item sx={{ padding: 1 }} >
           <Carousel show={5}>
             {this.props.recommendations.map((item) => (
-              <Card style={{ width: 320, height: 350, marginLeft: 15 }}>
-                <CardHeader title={`${item.creator}`} style={{ textAlign: 'center', backgroundColor: item.color }} />
-                <CardContent >
-                  <Grid container justifyContent='center' >
-                    <ImageStack imageData={item.imageData} />
-                  </Grid>
-                </CardContent>
+              <Card style={{ width: 340, height: 250, marginLeft: 10 }}>
+                <CardActionArea>
+                  <CardHeader title={`${item.creator}`} style={{ textAlign: 'center', backgroundColor: item.color }} />
+                  <CardContent >
+                    <Grid container justifyContent='center' >
+                      <ImageStack imageData={item.imageData} />
+                    </Grid>
+                  </CardContent>
+                </CardActionArea>
               </Card>
             ))}
           </Carousel>
