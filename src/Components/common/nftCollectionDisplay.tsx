fea83f09eaa9a66472f876f7ba68f5074b0be0e1
Harshal <harshalparab@gmail.com>
2022-05-22 19:32:28 +0530

    minor changes

--- src/Components/common/nftCollectionDisplay.tsx
+++ src/Components/common/nftCollectionDisplay.tsx
@@ -5,7 +5,9 @@ import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CardActionArea from '@mui/material/CardActionArea';
 import ImageStack from '../common/imageStack';
-import { Carousel } from '../customControls/carouselControl'
+import { Carousel } from '../customControls/carouselControl';
+import NftDetailDisplay from '../common/nftDetailDisplay';
+import ButtonBase from '@mui/material/ButtonBase';
 
 class NftCollectionDisplay extends React.Component<any, any> {
 
@@ -13,16 +15,47 @@ class NftCollectionDisplay extends React.Component<any, any> {
     super(props);
     this.state = {
       isLoading: false,
+      isDetailVisible: false,
+      selectedCreator: null,
+      selectedDetail: [],
       recommendations: [],
       title: null
     }
+    this.showDetails = this.showDetails.bind(this);
   }
 
   async componentDidMount() {
     this.setState({ isLoading: true })
     var recommendations = this.props.recommendations;
     var title = this.props.title;
-    this.setState({ recommendations: recommendations, isLoading: false })
+    this.setState({ recommendations: recommendations, isLoading: false })    
+  }
+
+  componentWillReceiveProps(props) {
+    {
+      this.refreshDetailVisible(false)
+    }
+  }
+
+  swapDetailVisible = () => this.setState({isDetailVisible: !this.state.isDetailVisible})
+
+  refreshDetailVisible = val => this.setState({isDetailVisible: val})
+
+  refreshSelectedDetail = details => this.setState({selectedDetail: details})
+
+  refreshSelectedCreator = creator => this.setState({selectedCreator: creator})
+
+  showDetails(item, event) {  
+    
+    if(this.state.selectedCreator == item.creator) {
+      this.swapDetailVisible();
+    }     
+    else {
+      this.refreshDetailVisible(true);
+    }
+    this.refreshSelectedCreator(item.creator);
+    this.refreshSelectedDetail(item);
+    
   }
 
   render() {
@@ -31,18 +64,23 @@ class NftCollectionDisplay extends React.Component<any, any> {
         <Grid container item sx={{ padding: 1 }} >
           <Carousel show={5}>
             {this.props.recommendations.map((item) => (
-              <Card style={{ width: 340, height: 250, marginLeft: 10 }}>
-                <CardActionArea>
-                  <CardHeader title={`${item.creator}`} style={{ textAlign: 'center', backgroundColor: item.color }} />
-                  <CardContent >
-                    <Grid container justifyContent='center' >
-                      <ImageStack imageData={item.imageData} />
-                    </Grid>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+              <div style={{ width: 340, height: 300, marginLeft: 10 }} > 
+                <Card onClick={(event) => this.showDetails(item, event)} >                  
+                  <CardActionArea>
+                    <CardHeader title={`${item.creator}`} style={{ textAlign: 'center', backgroundColor: item.color }} />
+                    <CardContent >
+                      <Grid container justifyContent='center' >
+                        <ImageStack imageData={item.imageData} />
+                      </Grid>
+                    </CardContent>
+                  </CardActionArea>                  
+                </Card>
+              </div>
             ))}
           </Carousel>
+          <div style={{alignContent: 'center', margin: 10 }}>
+            {this.state.isDetailVisible && <NftDetailDisplay selectedDetail={this.state.selectedDetail} /> }
+          </div>          
         </Grid>
       </div>
     )
