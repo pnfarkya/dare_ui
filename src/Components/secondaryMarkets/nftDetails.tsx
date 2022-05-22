7d9256f6dde6e8569c41b58aa03323f406bb81d9
pnfarkya <prineshfakya@gmail.com>
2022-05-22 10:07:44 +0530

    nft details page

--- src/Components/secondaryMarkets/nftDetails.tsx
+++ src/Components/secondaryMarkets/nftDetails.tsx
@@ -2,6 +2,8 @@ import { Grid, Typography, Box, Button } from "@mui/material";
 import StarIcon from '@mui/icons-material/Star';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LanguageIcon from '@mui/icons-material/Language';
 
 export default function NftDetails(props) {
     const { nftItem } = props;
@@ -14,8 +16,8 @@ export default function NftDetails(props) {
                         <img src={nftItem.image_thumbnail_url} style={{ width: "100px", height: "100px" }} />
                     </Grid>
                     <Grid item xs={12} sm={10}>
-                        <Typography fontWeight='bold' fontFamily='cursive' variant="body2" color="text.primary" fontWeight="bold"> {nftItem.name} </Typography>
-                        <Typography fontWeight='bold' fontFamily='cursive' variant="body2" color="text.secondary"> {nftItem.description}</Typography>
+                        <Typography fontWeight='bold' fontFamily='cursive' variant="body2" color="primary"> {nftItem.name} </Typography>
+                        <Typography fontWeight='bold' fontFamily='cursive' variant="body2" color="info"> {nftItem.description}</Typography>
                     </Grid>
                 </Grid>
 
@@ -26,11 +28,11 @@ export default function NftDetails(props) {
                         </Grid>
                         <Grid item xs={12} sm={3}>
 
-                            <Typography fontWeight='bold' fontFamily='cursive'><MonetizationOnIcon color="info" />{`( ${nftItem.convertValue})`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold' fontFamily='cursive'><MonetizationOnIcon color="info" />{`( ${nftItem.convertValue})`}</Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography fontWeight='bold'>{`24hr Volume: ${nftItem.volume}`}</Typography>
-                            <Typography fontWeight='bold'>{`24hr Sold Count: ${nftItem.count}`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold'>{`24hr Volume: ${nftItem.volume}`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold'>{`24hr Sold Count: ${nftItem.count}`}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
@@ -40,43 +42,43 @@ export default function NftDetails(props) {
                 <Grid item id="numbers" container>
                     <Grid item xs={12} sm={1.5}>
                         <Box sx={{ border: 1, alignContent: 'center', height: '90px', width: '100px', background: 'yellow' }}>
-                            <Typography fontWeight='bold' fontFamily='cursive' alignContent='center'>{`10K \n items`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold' fontFamily='cursive' alignContent='center'>{`10K \n items`}</Typography>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={1.5}>
                         <Box sx={{ border: 1, borderColor: 'yellow', alignContent: 'center', height: '90px', width: '100px', background: 'green' }}>
-                            <Typography fontWeight='bold' fontFamily='cursive' alignContent='center'>{`${nftItem.owners} \n owners`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold' fontFamily='cursive' alignContent='center'>{`${nftItem.owners} \n owners`}</Typography>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={1.5}>
                         <Box sx={{ border: 1, borderColor: 'green', alignContent: 'center', height: '90px', width: '100px', background: 'orange' }}>
-                            <Typography fontWeight='bold' fontFamily='cursive' alignContent='center'>{`${nftItem.floorPrice} \n floor price`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold' fontFamily='cursive' alignContent='center'>{`${nftItem.floorPrice} \n floor price`}</Typography>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={1.5}>
                         <Box sx={{ border: 1, borderColor: 'orange', alignContent: 'center', height: '90px', width: '100px', background: 'pink' }}>
-                            <Typography fontWeight='bold' fontFamily='cursive' style={{ alignSelf: 'center' }} alignContent='center'>{`${nftItem.tradedVolumes} \n\t traded volumes`}</Typography>
+                            <Typography textAlign='center' fontWeight='bold' fontFamily='cursive' style={{ alignSelf: 'center' }} alignContent='center'>{`${nftItem.tradedVolumes} \n\t traded volumes`}</Typography>
                         </Box>
                     </Grid>
                 </Grid>
                 <Grid id="traits" container item>
-                    <Grid item xs={12} sm={2}>
-                        <Box sx={{ border: 1, borderColor: 'pink', alignContent: 'center', borderColor: 'blue', background: '#ADD8E6', height: '120px', width: '120px' }}>
-                            <Typography alignContent='center' fontWeight='bold' fontFamily='cursive'>{`Background: \n ${nftItem.background}`}</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} sm={2}>
-                        <Box sx={{ border: 1, alignContent: 'center', borderColor: 'blue', background: '#ADD8E6', height: '120px', width: '120px' }}>
-                            <Typography alignContent='center' fontWeight='bold' fontFamily='cursive'>{`Eyes: \n ${nftItem.eyes}`}</Typography>
-                        </Box>
-                    </Grid>
+                    {
+                        nftItem.traits.map((t) =>
+                        (<Grid item xs={12} sm={2}>
+                            <Box sx={{ border: 1, borderColor: 'pink', alignContent: 'center', borderColor: 'blue', background: '#ADD8E6', height: '120px', width: '150px' }}>
+                                <Typography alignContent='center' textAlign='center' fontFamily='cursive'>{t.name}</Typography>
+                                <Typography alignContent='center' fontWeight='bold' textAlign='center' fontFamily='cursive'>{t.value}</Typography>
+                            </Box>
+                        </Grid>)
+                        )
+                    }
                 </Grid>
                 <Grid item id="social Media" container rowSpacing={5}>
-                    <Grid item xs={12} sm={3}>
-                        <a href={nftItem.webHandel}>{`Twitter: \n ${nftItem.twitterHandel}`}</a>
+                    <Grid item xs={12} sm={4}>
+                        <TwitterIcon color="primary" /><a href={nftItem.webHandel}>{nftItem.twitterHandle}</a>
                     </Grid>
-                    <Grid item xs={12} sm={5}>
-                        <a href={nftItem.webHandel}>{`Website: \n ${nftItem.webHandel}`}</a>
+                    <Grid item xs={12} sm={4}>
+                        <LanguageIcon color="secondary" /><a href={nftItem.webHandel}>{nftItem.webHandel}</a>
                     </Grid>
                 </Grid>
             </Grid>
@@ -119,50 +121,50 @@ export default function NftDetails(props) {
                 <Grid item container id="contactDetails">
                     <Grid item container>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Contact Address :</Typography>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Contact Address :</Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.address}`}</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item container>
-                        <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Token Id :</Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.tokenId}`}</Typography>
+                            <Typography textAlign='center' fontSize='20'>{`${nftItem.address}`}</Typography>
                         </Grid>
                     </Grid>
                     <Grid item container>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Token Standard :</Typography>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Token Id :</Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.standard}`}</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid item container>
-                        <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Blockchain :</Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.blockchain}`}</Typography>
+                            <Typography textAlign='center' textOverflow='auto' fontSize='20'>{`${nftItem.tokenId}`}</Typography>
                         </Grid>
                     </Grid>
                     <Grid item container>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Metadata :</Typography>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Token Standard :</Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.metadata}`}</Typography>
+                            <Typography textAlign='center' fontSize='20'>{`${nftItem.standard}`}</Typography>
                         </Grid>
                     </Grid>
                     <Grid item container>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='14' fontWeight='bold'>Creator Fees :</Typography>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Blockchain :</Typography>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Typography alignContent='center' fontSize='20'>{`${nftItem.creatorFees}`}</Typography>
+                            <Typography textAlign='center' fontSize='20'>{`${nftItem.blockchain}`}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item container>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Metadata :</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign='center' fontSize='20'>{`${nftItem.metadata}`}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item container>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign='center' fontSize='14' fontWeight='bold'>Creator Fees :</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Typography textAlign='center' fontSize='20'>{`${nftItem.creatorFees}`}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
